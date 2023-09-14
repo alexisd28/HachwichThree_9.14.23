3073bf8154f3d3147022434c62f35389159c9637
Alexis Decker <deckera@hawaii.edu>
2023-09-14 13:27:50 -1000

    completed part 3 if else statements

--- HachwichThree_9.14.23/ViewController.swift
+++ HachwichThree_9.14.23/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+    //please declare your global variables here
+    
+    var firstString = "The background color will turn blue"
+    var secondString = "The background color will turn green"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func changeColorButtonPressed(_ sender: Any)
+    
+    
+    {
+        if firstString == "The background color will turn green"
+        {
+            print(self.view.backgroundColor = UIColor.red)
+            
+        }
+        else
+        {
+        print(self.view.backgroundColor = UIColor.blue)
+    }
+    }
+    //print(
 }
-
